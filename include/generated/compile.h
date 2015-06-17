dafff735280db2aa359803afe71dac9c0211a0d2
engine95 <mustangsroc@gmail.com>
2015-06-17 15:08:39 -0600

    First commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Jun 12 21:26:31 MDT 2015"
+#define LINUX_COMPILE_BY "matt"
+#define LINUX_COMPILE_HOST "engine95-VBUbuntu"
+#define LINUX_COMPILER "gcc version 4.9.1 20140505 (prerelease) (crosstool-NG linaro-1.13.1-4.9-2014.05 - Linaro GCC 4.9-2014.05) "
