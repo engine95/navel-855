d03ba944d820d9feb0775b219c4088f8a69516a3
engine95 <mustangsroc@gmail.com>
2015-06-23 18:43:43 -0600

    kcal

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Fri Jun 12 21:26:31 MDT 2015"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Jun 23 13:54:06 MDT 2015"
 #define LINUX_COMPILE_BY "matt"
 #define LINUX_COMPILE_HOST "engine95-VBUbuntu"
 #define LINUX_COMPILER "gcc version 4.9.1 20140505 (prerelease) (crosstool-NG linaro-1.13.1-4.9-2014.05 - Linaro GCC 4.9-2014.05) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-perf"
+#define UTS_RELEASE "3.4.0-perf-gdafff73-dirty"
